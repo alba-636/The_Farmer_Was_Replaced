e0b6fbdc42fbf1af29717b663743fae1b1129696
Alba <29271954+alba-636@users.noreply.github.com>
2025-10-15 16:20:55 +0200

    feat: farm cactus with multiple drones

--- scripts/cactusOnly.py
+++ scripts/cactusOnly.py
@@ -1,10 +1,7 @@
 from __builtins__ import *
 from common import *
 
-helloWorld()
 clear()
-
-worldSize = 32
 
 def plantCactusOfSize(size):
   if get_ground_type() != Grounds.Soil:
@@ -13,26 +10,28 @@ def plantCactusOfSize(size):
     harvest()
     plant(Entities.Cactus)
 
-areAllSameSize = False
+def cactus():
+  size = 9
+  for _ in range(32):
+    plantCactusOfSize(size)
+    move(North)
+      
+staringPositions = buildStartingPositions(1, 32)
+
 while True:
-  goToPosition(0, 0)
-  if areAllSameSize and can_harvest():
-    harvest()
-    
-  areAllSameSize = True
+  drones = []
+  for i in range(len(staringPositions)):
+    if i == 31:
+      continue
+    goToPosition(staringPositions[i][0], staringPositions[i][1])
+    drone = spawn_drone(cactus)
+    drones.append(drone)
 
-  for i in range(worldSize):
-    for y in range(worldSize):
-      size = 9
-      plantCactusOfSize(size)
-      if measure() != size:
-        areAllSameSize = False
-      if y != worldSize - 1:
-        if isEven(i):
-          move(North)
-        else:
-          move(South)
+  goToPosition(31, 0)
+  cactus()
 
-    if i != worldSize - 1:
-      move(East)
-    
+  for i in range(len(drones)):
+    wait_for(drones[i])
+
+  sleep(5)
+  harvest()
