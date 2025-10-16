40ac068f948c9f64a276201c612d8cfb5a5b5e57
Alba <29271954+alba-636@users.noreply.github.com>
2025-10-16 17:00:51 +0200

    feat: better Hay leaderboard

--- leaderboards/hay.py
+++ leaderboards/hay.py
@@ -1,83 +1,62 @@
 # From: Alba-636
 # Leaderboard: Hay
-# Best time: 03:16:590
-# Rank: #17
-# At: 2025-10-15T15:35:59Z
-
+# Best time: 02:15:013
+# Rank: #11
+# At: 2025-10-16
 from __builtins__ import *
 from common import *
 
-clear()
-
-isTest = False
-companionPositions = [
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-  [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,],
-]
+set_world_size(24)
 
 def hasSuccess():
-  return not isTest and num_items(Items.Hay) >= 2000000000
+  return num_items(Items.Hay) >= 2000000000
 
-def farm(initialX, initialY):
+def farm(initialX, initialY, index):
+  farmSize = 4
+  xMax = 3
+  companionType = Entities.Bush
+  for x in range(farmSize):
+    for y in range(farmSize):
+      if isEven(x):
+        goToPosition(x + initialX, y + initialY)
+      else:
+        goToPosition(x + initialX, xMax - y + initialY)
+        
+      plant(companionType)
+
+  if index == 26 or index == 27 or index == 28 or index == 29:
+    offsetX = initialX + 3
+    offsetY = initialY + 4
+    for x in range(farmSize):
+      for y in range(farmSize):
+        if isEven(x):
+          goToPosition(offsetX - x, y + offsetY)
+        else:
+          goToPosition(offsetX - x, xMax - y + offsetY)
+          
+        plant(companionType)
+
+    goToPosition(3 + initialX, initialY)
+
   while True:
-    x = 3 + initialX
-    y = 3 + initialY
-
-    goToPosition(x, y)
-    
-    if can_harvest():
-      harvest()
-    useWater(0.9)
-
-    # Polyculture/compagnon
-    companionPlantType, (companionX, companionY) = get_companion()
-    if companionPositions[companionX][companionY] != companionPlantType:
-      goToPosition(companionX, companionY)
-      gatherAndPlant(companionPlantType)
-      companionPositions[companionX][companionY] = companionPlantType
-
     if hasSuccess():
       break
 
-staringPositions = buildStartingPositions(5, 5)
+    plantType, (_, _) = get_companion()
+    if can_harvest() or plantType != companionType:
+      harvest()
+    elif index == 0 and plantType == companionType and num_items(Items.Fertilizer) > 0:
+      use_item(Items.Fertilizer, 1)
+      if num_items(Items.Weird_Substance) > 0:
+        use_item(Items.Weird_Substance, 1)
+      harvest()
+    else:
+      useWater(0.95, 1)
 
+staringPositions = buildStartingPositions(4, 4)
 for i in range(len(staringPositions)):
   def drone():
-    farm(staringPositions[i][0], staringPositions[i][1])
-
+    farm(staringPositions[i][0], staringPositions[i][1], i)
   if i == 31:
     drone()
     break
