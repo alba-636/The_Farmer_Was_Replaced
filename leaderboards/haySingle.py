d8ad3ba2c91f3b08de16645dbf7a0c0782e23862
Alba <29271954+alba-636@users.noreply.github.com>
2025-10-17 09:49:14 +0200

    feat: better HaySingle leaderboard

--- leaderboards/haySingle.py
+++ leaderboards/haySingle.py
@@ -1,19 +1,19 @@
 # From: Alba-636
 # Leaderboard: Hay_Single
-# Best time: 03:03.987
-# Rank: #15
+# Best time: 02:57.539
+# Rank: #11
 # At: 2025-10-17
 
 from __builtins__ import *
 from common import *
 
-set_world_size(8)
+set_world_size(5)
 
 def hasSuccess():
   return num_items(Items.Hay) >= 100000000 # 100_000_000
 
-farmSize = 8
-xMax = 7
+farmSize = 5
+xMax = farmSize - 1
 companionType = Entities.Bush
 for x in range(farmSize):
   for y in range(farmSize):
@@ -22,9 +22,8 @@ for x in range(farmSize):
     else:
       goToPosition(x, xMax - y)
       
-    plant(companionType)
-
-goToPosition(3, 3)
+    if not (x == 0 and y == 4) and not (x == 4 and y == 4):
+      plant(companionType)
 
 isPosA = True
 
@@ -36,10 +35,11 @@ while True:
   if can_harvest() or plantType != companionType:
     harvest()
   elif not can_harvest() and plantType == companionType:
+    useWater(0.8, 4)
     if isPosA:
       isPosA = False
       move(East)
     else:
       isPosA = True
       move(West)
-    useWater(0.95, 1)
+
