62cc894680e9352f85da3fd5d39a79655f9e4d1f
Alba <29271954+alba-636@users.noreply.github.com>
2025-10-20 10:59:52 +0200

    feat: WoodSingle leaderboard

---
+++ leaderboards/woodSingle.py
@@ -0,0 +1,28 @@
+# From: Alba-636
+# Leaderboard: Wood_Single
+# Best time: 06:56.386
+# At: 2025-10-20
+
+# Avg: 6:2.1
+# Min: 5:41.7
+# Max: 6:14.41
+
+set_world_size(12)
+
+while True:
+  plantType, (_, _) = get_companion()
+  if can_harvest():
+    harvest()
+    plant(Entities.Tree)
+    if num_items(Items.Wood) >= 500000000:
+      break
+  elif plantType != Entities.Grass:
+    harvest()
+    plant(Entities.Tree)
+  else:
+    if get_water() < 0.64:
+      use_item(Items.Water, 4)
+    move(North)
+    move(East)
+
+
